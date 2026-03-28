e9dfc8225f7ae9f81a115ce2b81b8e43ac0acc36
Anga205 <github@angadbhalla.com>
2026-03-28 12:42:21 +0530

    allow cross origin requests from all origins

--- main.go
+++ main.go
@@ -3,11 +3,15 @@ package main
 import (
 	"CodeSandboxAPI/routes"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
+
+	router.Use(cors.Default())
+
 	routes.Setup(router)
 	router.Run()
 }
