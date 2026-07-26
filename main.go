184852b53813c04814a0055fabdfc85f9dd329cb
Anga205 <github@angadbhalla.com>
2026-07-26 20:00:17 +0530

    allow cross-origin requests from the ui

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"CodeSandboxAPI/routes"
+	"net/http"
+	"time"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
@@ -10,7 +12,18 @@ import (
 func main() {
 	router := gin.Default()
 
-	router.Use(cors.Default())
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"https://compiler.anga.codes"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
+	router.OPTIONS("/*path", func(c *gin.Context) {
+		c.Status(http.StatusNoContent)
+	})
 
 	routes.Setup(router)
 	router.Run()
